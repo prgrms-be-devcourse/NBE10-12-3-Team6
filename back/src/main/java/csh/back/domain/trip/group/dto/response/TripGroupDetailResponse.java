345b00fb86ba2e15afa0fda7d2c14ae62fd02db0
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-07-28 22:12:52 +0900

    feat/5/hotfix - tripGroup&tripMember ERD 통일 및 오타 수정

--- back/src/main/java/csh/back/domain/trip/group/dto/response/TripGroupDetailResponse.java
+++ back/src/main/java/csh/back/domain/trip/group/dto/response/TripGroupDetailResponse.java
@@ -1,7 +1,7 @@
 package csh.back.domain.trip.group.dto.response;
 
 import csh.back.domain.trip.group.entity.TripGroup;
-import csh.back.domain.trip.member.dto.response.TripMemeberResponse;
+import csh.back.domain.trip.member.dto.response.TripMemberResponse;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -15,9 +15,9 @@ public record TripGroupDetailResponse(
 		int nights,
 		LocalDate startDate,
 		LocalDate endDate,
-		List<TripMemeberResponse> members
+		List<TripMemberResponse> members
 ){
-	public static TripGroupDetailResponse from(TripGroup tripGroup, List<TripMemeberResponse> members) {
+	public static TripGroupDetailResponse from(TripGroup tripGroup, List<TripMemberResponse> members) {
 		return new TripGroupDetailResponse(
 				tripGroup.getId(),
 				tripGroup.getName(),
