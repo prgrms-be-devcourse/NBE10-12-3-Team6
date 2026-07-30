798cdda7a81e5ae92645a3debdeb80db9bb4900f
devho <jshgold93@gmail.com>
2026-07-30 13:53:50 +0900

    feat/20/dev-글로벌 도메인 테스트 및 최초 투표시 동시성 문제 해결

--- back/src/main/kotlin/csh/back/domain/vote/vote/repository/VoteRepository.kt
+++ back/src/main/kotlin/csh/back/domain/vote/vote/repository/VoteRepository.kt
@@ -2,7 +2,9 @@ package csh.back.domain.vote.vote.repository
 
 import csh.back.domain.trip.timeline.entity.Timeline
 import csh.back.domain.vote.vote.entity.Vote
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 import java.util.Optional
 
@@ -16,4 +18,8 @@ interface VoteRepository : JpaRepository<Vote, Long> {
 
     @Query("SELECT v.timeline FROM Vote v WHERE v.id = :voteId")
     fun findTimelineByVoteId(voteId: Long): Optional<Timeline>
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT v FROM Vote v WHERE v.id = :voteId")
+    fun findByIdWithLock(voteId: Long): Optional<Vote>
 }
