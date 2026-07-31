d7967733d11afa9e3aecedf24d2d8ee28076e1b4
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-08-01 01:05:07 +0900

    feat/32/dev - email 값 제외

--- back/src/main/kotlin/csh/back/domain/trip/member/dto/response/PastMateResponse.kt
+++ back/src/main/kotlin/csh/back/domain/trip/member/dto/response/PastMateResponse.kt
@@ -5,7 +5,6 @@ import java.time.LocalDate
 data class PastMateResponse(
     val id: Long,
     val name: String,
-    val email: String,
     val travelCount: Long,
     val latestTravelDate: LocalDate,
 )
