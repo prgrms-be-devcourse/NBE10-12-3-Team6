cff5312b9cabac2b6179d4628d8a85048fcf1e29
developerapp0721@gmail.com <developerapp0721@gmail.com>
2026-08-05 15:46:12 +0900

    fix: 보완

--- back/src/test/kotlin/csh/back/domain/trip/post/reminder/scheduler/PostReminderSchedulerTest.kt
+++ back/src/test/kotlin/csh/back/domain/trip/post/reminder/scheduler/PostReminderSchedulerTest.kt
@@ -14,6 +14,6 @@ class PostReminderSchedulerTest {
 
         scheduler.sendOneYearAgoPostReminders()
 
-        verify(service).sendOneYearAgoPostReminders()
+        verify(service).sendOneWeekAgoPostReminders()
     }
 }
